636f314f5c7bdb107664062de0d922d2885d7f9c
qushe8r <qushe8r@gmail.com>
2023-11-06 14:24:01 +0900

    Feat: @Retry 개발 (#7)

---
+++ demo/src/main/java/com/wanted/demo/global/aop/aspect/RetryAspect.java
@@ -0,0 +1,33 @@
+package com.wanted.demo.global.aop.aspect;
+
+import com.wanted.demo.global.aop.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+
+@Slf4j
+@Order(Ordered.LOWEST_PRECEDENCE - 1)
+@Aspect
+public class RetryAspect {
+
+    @Around("@annotation(retry)")
+    public Object doRetry(ProceedingJoinPoint joinPoint, Retry retry) throws Throwable {
+        log.info("[retry] {} retry={}", joinPoint.getSignature(), retry);
+        int maxRetry = retry.value();
+        Exception exceptionHolder = null;
+
+        for (int retryCount = 1; retryCount <= maxRetry; retryCount++) {
+            try {
+                log.info("[retry] try count={}/{}", retryCount, maxRetry);
+                return joinPoint.proceed();
+            } catch (Exception e) {
+                exceptionHolder = e;
+            }
+        }
+
+        throw exceptionHolder;
+    }
+}
